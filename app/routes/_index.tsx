3e98320dfaaa6e4f231e18c62458afa9804a7378
Hannes Bibel <hannes.bibel@gmail.com>
2024-10-19 15:01:15 +0200

    Add Zustand and implement history button behavior

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 
 import HistoryButton from "@/components/history-button";
+import { useApplicationState } from "@/state/state";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,10 +11,14 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const historyVisible = useApplicationState(s => s.ui.historyVisible);
+
   return (
     <div className="flex flex-col mt-1">
       <div className="flex justify-between">
         <HistoryButton />
+        {!historyVisible && "history is not visible"}
+        {historyVisible && "history is visible"}
       </div>
     </div>
   );
---
+++ app/state/state.ts
@@ -0,0 +1,7 @@
+import { create } from "zustand";
+
+import { State } from "./model";
+
+export const useApplicationState = create<State>(_set => ({
+  ui: { historyVisible: false },
+}));
